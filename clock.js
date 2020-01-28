056a8b94aa80da51ed719a64579b05ea2bad2ca6
CCLancaster <connieclancaster@gmail.com>
2020-01-27 19:05:41 -0800

    the perfect time, just in time

--- clock.js
+++ clock.js
@@ -4,16 +4,32 @@ let minute = document.getElementById("minute");
 let hour = document.getElementById("hour");
 
 
-var degrees = 45;
-count = 0
+var degrees = 0;
+let count = 0;
+
+let secondCount = 6
+let minuteCount = .06
+let hourCount = .00833333
+
+let secondRotation = 0;
+let minuteRotation = 0;
+let hourRotation = 0;
 
 setInterval(function() {
-seconds.style.transform = "rotate(" + (6 + count) + "deg)";
-count += 6;
+    
+    // secondRotation = secondCount / 60 * 360;
+    // minuteRotation = minuteCount / (60 * 60) * 360;
+    // hourRotation = hourCount / (60 * 60 * 12) * 360;
+
+    seconds.style.transform = "rotate(" + (secondRotation += secondCount) + "deg)";
+    minute.style.transform = "rotate(" + (minuteRotation += minuteCount) + "deg)";
+    hour.style.transform = "rotate(" + (hourRotation += hourCount) + "deg)";
+
+    console.log(secondRotation);
+
 }, 1000);
 
 
 setInterval(function(){
     console.log("TICK!");
  }, 1000);
-
